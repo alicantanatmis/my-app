7d1566cc08afcecc37591c60f94bdd828dd5dc86
Alican Tanatmis <alicantan60@gmail.com>
2014-12-03 09:06:15 -0500

    initial commit

---
+++ platforms/android/ant-build/CordovaApp-debug-unaligned.apk.d
@@ -0,0 +1,4 @@
+C:\my-app\platforms\android\ant-build\CordovaApp-debug-unaligned.apk : \
+C:\my-app\platforms\android\ant-build\CordovaApp.ap_ \
+C:\my-app\platforms\android\ant-build\classes.dex \
+C:\my-app\platforms\android\CordovaLib\ant-build\classes.jar \
---
+++ platforms/android/ant-build/classes.dex.d
@@ -0,0 +1,9 @@
+C:\my-app\platforms\android\ant-build\classes.dex : \
+C:\my-app\platforms\android\ant-build\classes\com\phonegap\helloworld\BuildConfig.class \
+C:\my-app\platforms\android\ant-build\classes\com\phonegap\helloworld\CordovaApp.class \
+C:\my-app\platforms\android\ant-build\classes\com\phonegap\helloworld\R$attr.class \
+C:\my-app\platforms\android\ant-build\classes\com\phonegap\helloworld\R$drawable.class \
+C:\my-app\platforms\android\ant-build\classes\com\phonegap\helloworld\R$string.class \
+C:\my-app\platforms\android\ant-build\classes\com\phonegap\helloworld\R$xml.class \
+C:\my-app\platforms\android\ant-build\classes\com\phonegap\helloworld\R.class \
+C:\my-app\platforms\android\ant-build\dexedLibs\classes-47e36d6c57361b987038191c853141f5.jar \
